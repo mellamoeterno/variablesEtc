471754a55cdd8a91f68d3b711b1cb127679e3d58
Enoque <thesonlike@gmail.com>
2023-09-23 23:29:35 -0300

    stuff

---
+++ array.js
@@ -0,0 +1,7 @@
+var myVar = [[[2],[3],[4],[5],[6],[7]],[[1],[3],[2]]];
+
+var firstElementFirstBracket = myVar[0][0];
+var thirdElementSecondBracket = myVar[1][2];
+
+console.log(firstElementFirstBracket);
+console.log(thirdElementSecondBracket);
--- testings.js
+++ testings.js
@@ -1,5 +1,25 @@
-var myArray = ["jhonny", 30, "giorno", 50];
+const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
 
-var myData = myArray[0];       
+console.log(plants.pop());
 
-console.log(myData);
+
+//In the code console.log(plants.pop()), 
+//the parentheses () after pop are used to actually call (execute) the pop method on the plants array.
+
+//in this case were removing and just doing console.log
+
+
+
+//we can also remove it and give its value to another variable
+//by doing:
+
+//var ourArray = [1,2,3];
+//var removedFromOurArray = ourArray.pop();
+
+//assigning the value to the "removedFromOurArray" variable.
+
+//.push .unshift -- .pop .shift --
+
+//two first ones adds and the two last ones removes, 
+//two adding ones returns length, will the removing ones returns value.
+
